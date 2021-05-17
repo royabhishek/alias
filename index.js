845ae7fbc1feb537784f2f2ac6b9c76f3f3ab09f
royabhishek <34426049+royabhishek@users.noreply.github.com>
2021-05-17 23:26:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = async function (context, req) {
+    const ogs = require("open-graph-scraper");
+    const options = { url: `https://${req.query.url}` };
+    const data = await ogs(options, (error, results, response) => {
+      return results; // This contains all of the Open Graph results
+    });
+    console.log(data);
+  
+    context.res = {
+      body: data,
+    };
+  };
